d121bbc224eb9092367884eddaea18ba6fa65403
Hikmat Rajabli <iamrajabli@outlook.com>
2022-09-02 16:08:13 +0400

    Tailwind конфиг и SCSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <button>Tailwind конфиг и SCSS</button>
     </h1>
   )
 }
